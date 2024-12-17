43df589e45ecf284e2585f1415a34258978fbccf
ErickAvellano <erickdevega.avellano@bicol-u.edu.ph>
2024-12-17 22:54:46 +0800

    `Refactored code in showmerchants.blade.php to remove unnecessary whitespace and indentation`

--- resources/views/stores/showmerchants.blade.php
+++ resources/views/stores/showmerchants.blade.php
@@ -189,22 +189,22 @@
                             <div class="text-start px-4">
                                 <div class="mb-2">
                                     @forelse ($shop->applications as $application)
-                                    @if (!empty($application->decoded_categories))
-                                        @foreach ($application->decoded_categories as $category)
-                                            @if (!empty($category))
-                                                @php
-                                                    $categoryName = str_replace(' Products', '', $category);
-                                                @endphp
-                                                <span class="badge bg-success">{{ $categoryName }}</span>
-                                            @endif
-                                        @endforeach
-                                    @else
-                                        <span class="badge bg-secondary">No Categories</span>
-                                    @endif
-                                @empty
-                                    <span class="badge bg-secondary">No Applications</span>
-                                @endforelse
-                                
+                                        @if (!empty($application->decoded_categories))
+                                            @foreach ($application->decoded_categories as $category)
+                                                @if (!empty($category))
+                                                    @php
+                                                        $categoryName = str_replace(' Products', '', $category);
+                                                    @endphp
+                                                    <span class="badge bg-success">{{ $categoryName }}</span>
+                                                @endif
+                                            @endforeach
+                                        @else
+                                            <span class="badge bg-secondary">No Categories</span>
+                                        @endif
+                                    @empty
+                                        <span class="badge bg-secondary">No Applications</span>
+                                    @endforelse
+                                </div>
                                 <!-- About Store -->
                                 <p class="mb-0">
                                     <strong>About Store:</strong> {{ Str::limit($shop->description, 100, '...') }}
