631a498cf62b4d4d58aff41a8a203843fcb91c4f
ErickAvellano <erickdevega.avellano@bicol-u.edu.ph>
2024-12-17 23:28:12 +0800

    `Removed 'Craft' and 'Crafts' from category name replacement in showmerchants.blade.php`

--- resources/views/stores/showmerchants.blade.php
+++ resources/views/stores/showmerchants.blade.php
@@ -194,7 +194,7 @@
                                                 @if (!empty($category))
                                                     @php
                                                         // Clean up the category name by removing "Products" or "Craft"
-                                                        $categoryName = str_replace(['Products', 'Craft', 'Crafts'], '', $category);
+                                                        $categoryName = str_replace(['Products'], '', $category);
 
                                                         // List of categories that should have "Crafts" appended
                                                         $categoriesWithCrafts = ['Rattan', 'Coconut', 'Leather', 'Karagumoy', 'Buri', 'Anahaw', 'Nito'];
