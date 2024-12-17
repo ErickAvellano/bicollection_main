666f625ad94ffa7b3648577c13953b331e6e7b52
ErickAvellano <erickdevega.avellano@bicol-u.edu.ph>
2024-12-17 21:31:18 +0800

    e

--- resources/views/Components/footer.blade.php
+++ resources/views/Components/footer.blade.php
@@ -1,7 +1,7 @@
 <style>
     /* General Footer Styles */
     .footer {
-        background-color: rgb(236, 234, 234), 252, 252);
+        background-color: rgb(236, 234, 234);
         text-align: center;
         font-family: Arial, sans-serif;
         padding: 20px 20px 0 20px;
