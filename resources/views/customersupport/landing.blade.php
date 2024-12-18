8b5ae0194761c3b996cac08f9c53439a35bb264c
ErickAvellano <erickdevega.avellano@bicol-u.edu.ph>
2024-12-18 13:59:18 +0800

    tres

--- resources/views/customersupport/landing.blade.php
+++ resources/views/customersupport/landing.blade.php
@@ -1,10 +1,28 @@
 @extends('Components.layout')
 
 @section('styles')
+<style>
+     main {
+            padding: 80px;
+            text-align: center;
+            background-image: url({{ asset('images/assets/start-selling/startsellingbg.png') }});
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            height:350px;
+        }
+
+</style>
 
 @endsection
 
 @section('content')
+<main>
+    <h1>Showcase and sell your unique Bicol products with ease!</h1>
+    <p class="mb-4">Crafted for your business success.</p>
+    <a href="{{ route('merchant.register') }}" class="cta-btn">Get Started Now</a>
+</main>
+
 
 
 @include('Components.footer')
