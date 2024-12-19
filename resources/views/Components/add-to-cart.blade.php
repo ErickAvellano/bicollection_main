16d469032c27ad4ed4bba377368596b090cd27ba
ErickAvellano <erickdevega.avellano@bicol-u.edu.ph>
2024-12-19 22:54:28 +0800

    test

--- resources/views/Components/add-to-cart.blade.php
+++ resources/views/Components/add-to-cart.blade.php
@@ -70,7 +70,8 @@
     </div>
 </div>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<meta name="csrf-token" content="{{ csrf_token() }}">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
 
 
 <script>
