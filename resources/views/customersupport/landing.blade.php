51221b09de765601cbc88b1282bd2bfdf00f9302
ErickAvellano <erickdevega.avellano@bicol-u.edu.ph>
2024-12-24 23:35:06 +0800

    awdadw

--- resources/views/customersupport/landing.blade.php
+++ resources/views/customersupport/landing.blade.php
@@ -4,6 +4,9 @@
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
 
+    body, html {
+        font-family: 'Poppins', sans-serif; /* Use Poppins font */
+    }
      main {
             padding: 80px;
             text-align: center;
