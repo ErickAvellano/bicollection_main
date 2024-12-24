088d12a3df133d0c4e3f2b986c6acd2bd7feca53
ErickAvellano <erickdevega.avellano@bicol-u.edu.ph>
2024-12-24 23:34:14 +0800

    wda

--- resources/views/customersupport/landing.blade.php
+++ resources/views/customersupport/landing.blade.php
@@ -2,7 +2,7 @@
 
 @section('styles')
 <style>
-    
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
 
      main {
             padding: 80px;
