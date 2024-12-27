9768255ef13526008d12c9645d5616037ce722e9
ErickAvellano <erickdevega.avellano@bicol-u.edu.ph>
2024-12-27 22:17:35 +0800

    adwdwa

--- resources/views/Components/admin-icon.blade.php
+++ resources/views/Components/admin-icon.blade.php
@@ -1,19 +1,4 @@
 <div id="support-add-icon" style="position: fixed;bottom: 20px;right: 20px;z-index: 9999;display: flex;gap: 10px;flex-direction: column-reverse;">
-    <a href="{{ route('customersupport.store-guide') }}" style="text-decoration: none;">
-        <div style="
-            width: 50px;
-            height: 50px;
-            background-color: #228b22;
-            color: white;
-            border-radius: 50%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.06);
-        ">
-           <i class="fa-solid fa-pen-to-square" style="font-size: 20px;"></i>
-        </div>
-    </a>
     <a href="" style="text-decoration: none;">
         <div style="
             width: 50px;
@@ -27,6 +12,21 @@
             box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.06);
         ">
             <i class="fas fa-headset" style="font-size: 20px;"></i>
+        </div>
+    </a>
+    <a href="{{ route('customersupport.store-guide') }}" style="text-decoration: none;">
+        <div style="
+            width: 50px;
+            height: 50px;
+            background-color: #228b22;
+            color: white;
+            border-radius: 50%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.06);
+        ">
+           <i class="fa-solid fa-pen-to-square" style="font-size: 20px;"></i>
         </div>
     </a>
 </div>
