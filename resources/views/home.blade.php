12b15b96f8776f5255e9c89b7bd66512d2d24cef
ErickAvellano <erickdevega.avellano@bicol-u.edu.ph>
2025-01-03 00:31:03 +0800

    awddaw

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -699,11 +699,8 @@
         .shop-contact i {
             font-size: 0.8rem; /* Adjust icon size for smaller screens */
         }
-        .product-card-hover .card-body {
-            padding: 8px; /* Adjust padding for small screens */
-        }
         .featured-title {
-            font-size: 0.85rem; /* Smaller font size for title */
+            font-size: 0.8rem; /* Smaller font size for title */
         }
         .card-reviews i {
             font-size: 0.75rem; /* Adjust star size */
